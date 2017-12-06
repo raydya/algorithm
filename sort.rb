2683c5ab232e525265e26d13cfb483a4c95a46e4
raydya <dya.ilaempire@gmail.com>
2017-12-06 22:46:01 +0800

    heap sort

--- sort.rb
+++ sort.rb
@@ -10,19 +10,48 @@ class Array
     end
     self
   end
+
+  def quick_sort!
+    return self if size < 2
+    pivot = first
+    left, right = self[1..-1].partition { |ele| ele <= pivot }
+    left.quick_sort! + [pivot] + right.quick_sort!
+  end
+
+  def swap(x, y)
+    tmp = self[x]
+    self[x] = self[y]
+    self[y] = tmp
+  end
+
+  def max_heapify(start_at, end_at)
+    dad = start_at
+    son = dad * 2 + 1
+    return if son >= end_at
+    son += 1 if son + 1 < end_at && self[son] < self[son + 1]
+    if self[dad] <= self[son]
+      swap(dad, son)
+      max_heapify(son, end_at)
+    end
+  end
+
+  def heap_sort!
+    (size / 2 - 1).downto(0) do |i|
+      max_heapify(i, size)
+    end
+    (size - 1).downto(1) do |i|
+      swap(0, i)
+      max_heapify(0, i)
+    end
+    self
+  end
 end
 
-def quick_sort_func(array)
-  return array if array.size < 2
-  pivot = array.first
-  left, right = array[1..-1].partition { |ele| ele <= pivot }
-  quick_sort_func(left) + [pivot] + quick_sort_func(right)
-end
-
-array = Array.new(10_000) { rand(1..9999) }
+array = Array.new(100_000) { rand(1..9_999) }
 
 Benchmark.bmbm do |x|
   x.report('default') { array.dup.sort }
-  x.report('bubble') { array.dup.bubble_sort! }
-  x.report('quick')  { quick_sort_func(array.dup) }
+  # x.report('bubble') { array.dup.bubble_sort! }
+  x.report('quick') { array.dup.quick_sort! }
+  x.report('heap') { array.dup.heap_sort! }
 end
